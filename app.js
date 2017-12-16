15eb06a0094e518e00fd9103ac5eb47785c13475
Evan Zhao <etz3@cornell.edu>
2017-12-15 20:28:35 -0800

    read env

--- app.js
+++ app.js
@@ -33,9 +33,9 @@ exports.callSendAPI = function(sender_psid, response) {
 	}
 
 	// Send the HTTP request to the Messenger Platform
-	request.request({
+	request({
 		"uri": "https://graph.facebook.com/v2.6/me/messages",
-		"qs": { "access_token": PAGE_ACCESS_TOKEN },
+		"qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
 		"method": "POST",
 		"json": request_body
 	}, (err, res, body) => {
