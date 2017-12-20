03f134d478492a7305387e665c0c1e6f57589fa0
Evan Zhao <etz3@cornell.edu>
2017-12-20 00:06:10 -0800

    lksdfj

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.post('/webhook', (req, res) => {
       console.log(webhookEvent);
 
       let sender_psid = webhookEvent.sender.id;
-      let sender_psid = 1337;
+      sender_psid = 1337;
       console.log("Sender psid: " + sender_psid);
 
 		// Check if the event is a message or postback and
