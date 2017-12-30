0eb68cff8e489bfe15b741658f48fcd43aab1172
Evan Zhao <etz3@cornell.edu>
2017-12-30 12:30:47 -0800

    test

--- app.js
+++ app.js
@@ -9,123 +9,99 @@ const
 // Handles messages events
 // Note that psid is the page scoped id. identifies users.
 exports.handleMessage = function(sender_psid, received_message, User) {
-	var callback = function (psid, message, User) {
+	let generateTextResponse = function (res) {
+		return response = {
+			"text": res
+		}
+	}
+
+	let checkEntity = function(data, input) {
+		return data.entities.intent && (data.entities.intent[0].value == input);
+	} 
+
+	let callback = function (psid, message, caller) {
 		let response;
 
 		client.message(message.text, {}).then((data) => {
 			console.log('Message contents: ' + JSON.stringify(data));
-			if (data.entities.intent && data.entities.intent[0].value == "start_standup") {
-				response = {
-					"text": `Beginning standup!`
-				}
+			if (checkEntity(data, "start_standup")) {
+
+				response = generateTextResponse("Beginning standup!");
 				console.log("WE SHOULD START AYYY");
-			} else {
-				if(data.entities.intent && data.entities.intent[0].value == "previous") {
-					var user_query = User.findOne({ psid: sender_psid });
-					user_query.then(function (this_user) {
-						if (!this_user) {
-							response = {
-								"text": "user not found"
-							}
-						} else {
-							console.log(this_user);
-							response = {
-								"text": `blockers were: ${this_user.blockers}`
-							}
-						}
-						console.log(response);
-						exports.callSendAPI(psid, response);
-						return;
+
+			} else if (checkEntity(data, "previous")) {
+				response = generateTextResponse(`blockers were: ${caller.blockers}`)
+
+			} else if (checkEntity(data, "blocker")) {
+				var arr = [];
+
+				if (data.entities.reminder) {
+					data.entities.reminder.forEach(function (entry) {
+						arr.push(entry.value);
 					});
 				}
-				// Filtering for understanding what type of intent is being used.
-				else if (data.entities.intent && data.entities.intent[0].value == "blocker") {
-					var arr = [];
 
-					if (data.entities.reminder) {
-						data.entities.reminder.forEach(function (entry) {
-							arr.push(entry.value);
-						});
-					}
-
-					if (arr.length == 0) {
-						User.update({ psid: psid }, { blockers: "No blockers poop" }, function(err, raw) {
-							if(err) console.log(err);
-							response = {
-								"text": `No blockers`
-							}
-							exports.callSendAPI(psid, response);
-							return;
-						});
-						response = {
-							"text": `No blockers`
-						}
-					} else {
-						let all = arr.join(', ');
-						User.update({ psid: psid }, { blockers: all }, function(err, raw) {
-							if(err) return console.error(err);
-						});
-						response = {
-							"text": `Blocked by: ${all}`
-						}
-					}
-
-				} else if (data.entities.intent && data.entities.intent[0].value == "today") {
-					var arr = [];
-
-					if (data.entities.reminder) {
-						data.entities.reminder.forEach(function (entry) {
-							arr.push(entry.value);
-						});
-					}
-
-					let temp = arr.join(', ');
-
-					response = {
-						"text": `Tasks for today are: ${temp}`
-					}
-				} else if (data.entities.intent && data.entities.intent[0].value == "yesterday") {
-					var arr = [];
-
-					if (data.entities.reminder) {
-						data.entities.reminder.forEach(function (entry) {
-							arr.push(entry.value);
-						});
-					}
-
-					let temp = arr.join(', ');
-
-					response = {
-						"text": `Yesterday I completed: ${temp}`
-					}
+				if (arr.length == 0) {
+					caller.blockers = "No blockers [beta version]";
+					response = generateTextResponse("No blockers");
 				} else {
-					// Create the payload for a basic text message
-					response = {
-						"text": `You sent the message: "${message.text}". Now send me an image!`
-					}	
+					let all = arr.join(', ');
+					caller.blockers = all;
+					response = generateTextResponse(`Blocked by: ${all}`)
 				}
+
+			} else if (checkEntity(data, "today")) {
+				var arr = [];
+
+				if (data.entities.reminder) {
+					data.entities.reminder.forEach(function (entry) {
+						arr.push(entry.value);
+					});
+				}
+
+				let temp = arr.join(', ');
+
+				response = generateTextResponse(`Tasks for today are: ${temp}`);
+			} else if (checkEntity(data, "yesterday")) {
+				var arr = [];
+
+				if (data.entities.reminder) {
+					data.entities.reminder.forEach(function (entry) {
+						arr.push(entry.value);
+					});
+				}
+
+				let temp = arr.join(', ');
+
+				response = generateTextResponse(`Yesterday I completed: ${temp}`);
+			} else {
+				// Create the payload for a basic text message
+				response = generateTextResponse(`You sent the message: "${message.text}". Now send me an image!`);
 			}
-			exports.callSendAPI(psid, response);
+			caller.save(function(err, updated_user) {
+				if (err) return console.error(err);
+				console.log("UPDATE: " + updated_user);
+				exports.callSendAPI(psid, response);
+			});
 		});
 	}
 
 	// Check if the message contains text
 	if (received_message.text) {
-
 		console.log(received_message.text);
-		callback(sender_psid, received_message, User);
+		// callback(sender_psid, received_message, User);
 
-		// var user_query = User.findOne({ psid: sender_psid });
-		// user_query.then(function (this_user) {
-		// 	if (!this_user) {
-		// 		new User({ psid: sender_psid }).save(function(err, user) {
-		// 			if (err) return console.error(err);
-		// 			callback(sender_psid, received_message, user);
-		// 		});
-		// 	} else {
-		// 		callback(sender_psid, received_message, this_user);
-		// 	}
-		// });
+		var user_query = User.findOne({ psid: sender_psid });
+		user_query.then(function (this_user) {
+			if (!this_user) {
+				User.create({ psid: sender_psid }, function(err, user) {
+					if (err) return console.error(err);
+					callback(sender_psid, received_message, user);
+				});
+			} else {
+				callback(sender_psid, received_message, this_user);
+			}
+		});
 
 	} else if (received_message.attachments) {
 		// Get the URL of the message attachment
